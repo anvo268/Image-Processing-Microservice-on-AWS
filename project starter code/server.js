8847dd0915b72af760a5c17af3b7fb7caaa1191e
Andrew Voorhees <andrewvoorhees@AndrewsacStudio.attlocal.net>
2024-05-16 12:13:38 -0700

    First commit

--- project starter code/server.js
+++ project starter code/server.js
@@ -1,17 +1,23 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util.js';
+import path from 'path';
 
+  console.log("Entered the server.js file!!");
 
 
   // Init the Express application
   const app = express();
+
+  console.log("Express App initiated!");
 
   // Set the network port
   const port = process.env.PORT || 8082;
   
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
+
+  console.log("Body parser thing worked!");
 
   // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
   // GET /filteredimage?image_url={{URL}}
@@ -29,13 +35,53 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util.js';
 
     /**************************************************************************** */
 
-  //! END @TODO1
-  
-  // Root Endpoint
-  // Displays a simple message to the user
-  app.get( "/", async (req, res) => {
-    res.send("try GET /filteredimage?image_url={{}}")
-  } );
+    const router = express.Router();
+      router.get("/filteredimage/", async (req, res) => {
+          let { image_url } = req.query
+
+          // Check if image_url is provided
+          if (!image_url) {
+            return res.status(400).send({ error: 'image_url query parameter is required' });
+          }
+
+           // Check if image_url is a valid URL
+          try {
+            new URL(image_url);
+          } catch (e) {
+            return res.status(400).send({ error: 'image_url query parameter must be a valid URL' });
+          }
+
+          // Check if image_url points to an image by checking the file extension
+          const extension = path.extname(new URL(image_url).pathname).toLowerCase();
+          if (!['.png', '.jpg', '.jpeg', '.gif'].includes(extension)) {
+            return res.status(400).send({ error: 'image_url must point to an image (jpeg, png, etc.)' });
+          }
+
+          // Download the filtered image locally and store the new path in filteredPath
+          const filteredPath = await filterImageFromURL(image_url);
+
+          console.log("File downloaded?");
+          console.log("Filtered Path: ", filteredPath);
+
+          // Send the filtered image back to the client
+          res.status(200).sendFile(filteredPath);
+
+          console.log("File sent?");
+
+          // Delete the downloaded image from the server
+          res.on('finish', () => deleteLocalFiles([filteredPath]));
+
+      })
+    
+    app.use(router)
+
+    //! END @TODO1
+    
+    // Root Endpoint
+    // Displays a simple message to the user
+    app.get( "/", async (req, res) => {
+      res.send("try GET /filteredimage?image_url={{}}")
+    } );
   
 
   // Start the Server
